7aa8b83059d4b6c77a9c22fb9adbd4faa1cacf42
Payton Swick <payton@foolord.com>
2025-05-04 17:48:02 -0400

    Add more helpers for creating episodes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use chrono::NaiveDateTime;
 use regex::Regex;
 use reqwest::blocking::Client;
 use reqwest::header::CONTENT_LENGTH;
+use scraper::{ElementRef, Selector};
 use std::error::Error;
 use std::fs::File;
 use xml::writer::{EmitterConfig, EventWriter, XmlEvent};
-use scraper::{Selector, ElementRef};
 
 #[derive(Debug)]
 struct Episode {
@@ -48,15 +48,15 @@ fn main() {
 
 fn prepare_constants() -> Constants {
     Constants {
-    episode_selector : scraper::Selector::parse(".fbody5 tr").unwrap(),
-    episode_title_selector : scraper::Selector::parse("td:first-of-type").unwrap(),
-    episode_date_selector : scraper::Selector::parse("td:nth-of-type(2)").unwrap(),
-    episode_url_selector : scraper::Selector::parse("td:nth-of-type(3) a").unwrap(),
-    episode_year_selector : scraper::Selector::parse("td:nth-of-type(3) a.archives").unwrap(),
-    date_format : "%a %b %d %I:%M %P %Y",
-    year_re : Regex::new(r"_(\d{4})\d+$").unwrap(),
-    file_name_re : Regex::new(r"\('([^']+)'").unwrap(),
-    url_path_prefix : "http://wmbr.org/archive/",
+        episode_selector: scraper::Selector::parse(".fbody5 tr").unwrap(),
+        episode_title_selector: scraper::Selector::parse("td:first-of-type").unwrap(),
+        episode_date_selector: scraper::Selector::parse("td:nth-of-type(2)").unwrap(),
+        episode_url_selector: scraper::Selector::parse("td:nth-of-type(3) a").unwrap(),
+        episode_year_selector: scraper::Selector::parse("td:nth-of-type(3) a.archives").unwrap(),
+        date_format: "%a %b %d %I:%M %P %Y",
+        year_re: Regex::new(r"_(\d{4})\d+$").unwrap(),
+        file_name_re: Regex::new(r"\('([^']+)'").unwrap(),
+        url_path_prefix: "http://wmbr.org/archive/",
     }
 }
 
@@ -65,39 +65,99 @@ fn get_html() -> String {
     response.unwrap().text().unwrap()
 }
 
-fn get_title_from_html(constants: &Constants, html_episode: ElementRef) -> Result<String, Box<dyn Error>> {
-        let title_element = html_episode.select(&constants.episode_title_selector).next();
-         match title_element {
-            Some(title_element) => Ok(title_element.text().collect::<Vec<_>>().join("")),
-            None => Err(Box::from("Failed to find title in html")),
-        }
+fn get_title_from_html(
+    constants: &Constants,
+    html_episode: ElementRef,
+) -> Result<String, Box<dyn Error>> {
+    let title_element = html_episode
+        .select(&constants.episode_title_selector)
+        .next();
+    match title_element {
+        Some(title_element) => Ok(title_element.text().collect::<Vec<_>>().join("")),
+        None => Err(Box::from("Failed to find title in html")),
+    }
 }
 
-fn get_date_from_html(constants: &Constants, html_episode: ElementRef) -> Result<NaiveDateTime, Box<dyn Error>> {
-        let date_element = html_episode.select(&constants.episode_date_selector).next();
-        let date_text = match date_element {
-            Some(date_element) => date_element.text().collect::<Vec<_>>().join(""),
-                None => return Err(Box::from("Failed to find date in html")),
-        };
-        let year_element = html_episode.select(&constants.episode_year_selector).next();
-        let year_text = match year_element {
-            Some(year_element) => match year_element.attr("id") {
-                Some(year_text) => year_text,
-                None => return Err(Box::from("Failed to find year ID in html")),
-            },
-            None => return Err(Box::from("Failed to find year in html")),
-        };
-        let year_captures = match constants.year_re.captures(year_text) {
-            Some(year_captures) => year_captures,
-            None => return Err(Box::from("Failed to find year number in year text")),
-        };
-        let year = &year_captures[1];
-        if year.is_empty() {
-            return Err(Box::from("Year number was empty in year text"));
+fn get_date_from_html(
+    constants: &Constants,
+    html_episode: ElementRef,
+) -> Result<NaiveDateTime, Box<dyn Error>> {
+    let date_element = html_episode.select(&constants.episode_date_selector).next();
+    let date_text = match date_element {
+        Some(date_element) => date_element.text().collect::<Vec<_>>().join(""),
+        None => return Err(Box::from("Failed to find date in html")),
+    };
+    let year_element = html_episode.select(&constants.episode_year_selector).next();
+    let year_text = match year_element {
+        Some(year_element) => match year_element.attr("id") {
+            Some(year_text) => year_text,
+            None => return Err(Box::from("Failed to find year ID in html")),
+        },
+        None => return Err(Box::from("Failed to find year in html")),
+    };
+    let year_captures = match constants.year_re.captures(year_text) {
+        Some(year_captures) => year_captures,
+        None => return Err(Box::from("Failed to find year number in year text")),
+    };
+    let year = &year_captures[1];
+    if year.is_empty() {
+        return Err(Box::from("Year number was empty in year text"));
+    }
+    let date_year = year;
+    let date_text_with_year = date_text + " " + date_year;
+    Ok(NaiveDateTime::parse_from_str(
+        &date_text_with_year,
+        &constants.date_format,
+    )?)
+}
+
+fn get_url_from_html(
+    constants: &Constants,
+    html_episode: ElementRef,
+) -> Result<String, Box<dyn Error>> {
+    let url_element = html_episode.select(&constants.episode_url_selector).next();
+    let url_onclick_text = match url_element {
+        Some(url_element) => match url_element.attr("onclick") {
+            Some(url_onclick_text) => url_onclick_text,
+            None => {
+                return Err(Box::from("Failed to find url onclick attr"));
+            }
+        },
+        None => {
+            return Err(Box::from("Failed to find url element in html"));
         }
-        let date_year = year;
-        let date_text_with_year = date_text + " " + date_year;
-        Ok(NaiveDateTime::parse_from_str(&date_text_with_year, &constants.date_format)?)
+    };
+    let file_name_captures = match constants.file_name_re.captures(url_onclick_text) {
+        Some(file_name_captures) => file_name_captures,
+        None => {
+            return Err(Box::from("Failed to find file name in url onclick text"));
+        }
+    };
+    let file_name = &file_name_captures[1];
+    if file_name.is_empty() {
+        return Err(Box::from("Url filename was empty"));
+    }
+    Ok(constants.url_path_prefix.to_owned() + file_name)
+}
+
+/// Return the file length in bytes
+fn get_file_length(url: &str) -> Result<usize, Box<dyn Error>> {
+    let client = Client::new();
+
+    let response = client.head(url).send()?;
+
+    if let Some(content_length) = response.headers().get(CONTENT_LENGTH) {
+        if let Ok(content_length_str) = content_length.to_str() {
+            if let Ok(content_length) = content_length_str.parse::<usize>() {
+                return Ok(content_length);
+            } else {
+                return Err(Box::from("Failed to parse content length"));
+            }
+        } else {
+            return Err(Box::from("Couldn't convert content length to str"));
+        }
+    }
+    return Err(Box::from("Content length header not found"));
 }
 
 macro_rules! skip_on_err {
@@ -118,60 +178,19 @@ fn get_episodes(html_content: &str) -> Vec<Episode> {
     let constants = prepare_constants();
     let html_episodes = document.select(&constants.episode_selector);
     for html_episode in html_episodes {
-        let title_text = skip_on_err!(get_title_from_html(&constants, html_episode));
+        let title = skip_on_err!(get_title_from_html(&constants, html_episode));
         let date = skip_on_err!(get_date_from_html(&constants, html_episode));
-        let url_element = html_episode.select(&constants.episode_url_selector).next();
-        let url_onclick_text = match url_element {
-            Some(url_element) => match url_element.attr("onclick") {
-                Some(url_onclick_text) => url_onclick_text,
-                None => {
-                    continue;
-                }
-            },
-            None => continue,
-        };
-        let file_name_captures = match constants.file_name_re.captures(url_onclick_text) {
-            Some(file_name_captures) => file_name_captures,
-            None => continue,
-        };
-        let file_name = &file_name_captures[1];
-        if file_name.is_empty() {
-            continue;
-        }
-        let url_text = constants.url_path_prefix.to_owned() + file_name;
-        let length = match get_file_length(&url_text) {
-            Ok(length) => length,
-            Err(_) => continue,
-        };
+        let url = skip_on_err!(get_url_from_html(&constants, html_episode));
+        let length = skip_on_err!(get_file_length(&url));
         let episode = Episode {
-            title: title_text,
+            title,
             date,
-            url: url_text,
+            url,
             length,
         };
         episodes.push(episode);
     }
     episodes
-}
-
-/// Return the file length in bytes
-fn get_file_length(url: &str) -> Result<usize, Box<dyn Error>> {
-    let client = Client::new();
-
-    let response = client.head(url).send()?;
-
-    if let Some(content_length) = response.headers().get(CONTENT_LENGTH) {
-        if let Ok(content_length_str) = content_length.to_str() {
-            if let Ok(content_length) = content_length_str.parse::<usize>() {
-                return Ok(content_length);
-            } else {
-                return Err(Box::from("Failed to parse content length."));
-            }
-        } else {
-            return Err(Box::from("Couldn't convert content length to str."));
-        }
-    }
-    return Err(Box::from("Content length header not found."));
 }
 
 /// Return the approximate number of seconds for the episode
